21ad844cbd3a7cfa50f18883e7da67df3bf8d193
Davidflo1648 <144282155+Davidflo1648@users.noreply.github.com>
2023-10-13 12:48:41 -0700

    task2 done

--- task2.py
+++ task2.py
@@ -9,3 +9,18 @@ There will be a maximum of 3 guesses allowed
 
 expectedUsername = "systemadmin"
 expectedPassword = "master"
+
+attempts = 0
+max_attempts = 3
+
+for i in range(max_attempts):
+    username = input("Enter a username: ")
+    password = input("Enter a password: ")
+
+    if username == expectedUsername and password == expectedPassword:
+        print("Access Granted.")
+        break
+    else:
+        print("Access Denied.")
+else:
+    print("You got locked out.")
