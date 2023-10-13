d666c47eea3881f874556905a19a9f7d31921c48
Davidflo1648 <144282155+Davidflo1648@users.noreply.github.com>
2023-10-13 12:53:51 -0700

    task3 done

--- task3.py
+++ task3.py
@@ -12,3 +12,17 @@ etc.
 
 users = ["annie","betty","charles","doug","eddie","flon"]
 passwords = ["12345","password","iloveyou","mom","default","0"]
+
+username = input("Enter your username: ")
+
+if username in users:
+    index = users.index(username)
+
+    password = input("Enter your password: ")
+
+    if password == passwords[index]:
+        print("Login successful!")
+    else:
+        print("Incorrect password. Login failed.")
+else:
+    print("Username not found. Login failed.")
