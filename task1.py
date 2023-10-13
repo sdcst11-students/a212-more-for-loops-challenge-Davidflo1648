9f4509ac677a4be438c56f58b99f394308349f6e
Davidflo1648 <144282155+Davidflo1648@users.noreply.github.com>
2023-10-13 12:40:19 -0700

    task1 done

--- task1.py
+++ task1.py
@@ -8,3 +8,21 @@ The program will then tell the user if the number is too high, too low or correc
 If the number is correct, the program will end
 If the 10 guesses are used up, the program will say that the user has lost
 '''
+import random
+number = random.randint(1, 100)
+attempts = 0
+max_attempts = 10
+
+for i in range(max_attempts):
+    guess = int(input("Enter a number: "))
+
+    if guess < number:
+        print("Too low. Try again!")
+    elif guess > number:
+        print("Too high. Try again!")
+    else:
+        print(f"Congratulations! You guessed the number, which is {number}!")
+        break
+else:
+    print(f"Game Over! \nYou failed all your attemps, the correct number was {number}")
+
